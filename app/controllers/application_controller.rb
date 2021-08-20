123e59c2e52b78a690bc0793b75cebabfa19152a
rodrigo <rob.developer@protonmail.com>
2021-08-20 13:42:05 -0400

    add name and photo attributes to user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permit_params, if: :devise_controller?
+
+    def configure_permit_params
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :photo])
+    end
+
 end
