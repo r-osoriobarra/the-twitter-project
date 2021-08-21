64cd59516d5bae5d6c9309386b0f602389111a40
rodrigo <rob.developer@protonmail.com>
2021-08-21 17:01:51 -0400

    add methods to tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,22 +1,39 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = Tweet.all
   end
 
   def show
+    @tweet = Tweet.find(params[:id])
   end
 
   def new
+    @tweet = Tweet.new
+    @user = current_user
   end
 
   def create
+    @tweet = Tweet.create(tweet_params)
+    @user = current_user
+
+    @tweet.user = current_user
+    @tweet.user_photo = @user.photo
+    # @tweet.n_retweets = 
+    # @tweet.n_likes =
+
+      respond_to do |format|
+        if @tweet.save
+          format.html { redirect_to root_path, notice: "Tweet was successfully created." }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+        end
+      end
   end
 
-  def edit
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:content)    
   end
 
-  def update
-  end
-
-  def destroy
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,7 @@ Rails.application.routes.draw do
   get 'likes/new'
   get 'likes/create'
   get 'likes/destroy'
-  get 'tweets/index'
-  get 'tweets/show'
-  get 'tweets/new'
-  get 'tweets/create'
-  get 'tweets/edit'
-  get 'tweets/update'
-  get 'tweets/destroy'
+  
   devise_for :users
   resources :users do
     resources :tweets
